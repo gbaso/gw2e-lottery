ee184cb1311cef9492e3a19df1c235648dac0b58
Giacomo Baso <ganondolf@gmail.com>
2022-06-14 16:51:15 +0200

    indent with spaces

--- src/main/java/com/github/gbaso/gw2elottery/Gw2eLotteryApplication.java
+++ src/main/java/com/github/gbaso/gw2elottery/Gw2eLotteryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Gw2eLotteryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Gw2eLotteryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Gw2eLotteryApplication.class, args);
+    }
 
 }
