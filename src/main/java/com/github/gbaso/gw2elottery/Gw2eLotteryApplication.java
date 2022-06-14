0752f55539e252641b735582e2dbe55bebeb536e
Giacomo Baso <ganondolf@gmail.com>
2022-06-14 16:39:08 +0200

    add project

---
+++ src/main/java/com/github/gbaso/gw2elottery/Gw2eLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.gw2elottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gw2eLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gw2eLotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gbaso/gw2elottery/Gw2eLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.gw2elottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gw2eLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
