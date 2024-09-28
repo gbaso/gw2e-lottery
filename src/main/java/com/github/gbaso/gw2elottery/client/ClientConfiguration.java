b7327755f8a39170d29c72b56c8ddd37df992ada
Giacomo Baso <gbaso@users.noreply.github.com>
2024-09-28 15:54:18 +0200

    formatting

--- src/main/java/com/github/gbaso/gw2elottery/client/ClientConfiguration.java
+++ src/main/java/com/github/gbaso/gw2elottery/client/ClientConfiguration.java
@@ -3,7 +3,6 @@ package com.github.gbaso.gw2elottery.client;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-
 @EnableConfigurationProperties(Gw2Properties.class)
 @Configuration(proxyBeanMethods = false)
 class ClientConfiguration {}
