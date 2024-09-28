aa49b24b39294c46ea970ae9fbc347c3674fc8c6
Giacomo Baso <gbaso@users.noreply.github.com>
2024-09-28 14:08:11 +0200

    annotate non-public repositories with @RepositoryRestResource

--- src/main/java/com/github/gbaso/gw2elottery/enterer/AccountRepository.java
+++ src/main/java/com/github/gbaso/gw2elottery/enterer/AccountRepository.java
@@ -1,5 +1,7 @@
 package com.github.gbaso.gw2elottery.enterer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 interface AccountRepository extends MongoRepository<Account, String> {}
